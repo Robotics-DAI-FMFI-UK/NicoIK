5b74a178ededac8ea0e7abe50b5b7da7dc88475a
Michal Vavrecka <michal.vavrecka@cvut.cz>
2024-11-29 09:53:11 +0100

    tiago update

--- calibration_matrices.py
+++ calibration_matrices.py
@@ -68,6 +68,37 @@ class TargetGrid:
 
         return round(self.x, 2), round(self.y, 2), round(self.z, 3)
 
+class TargetGridTiago:
+    def __init__(self):
+        self.x_start, self.x_end, self.x_step = 0.2, 0.8, 0.1
+        self.y_start, self.y_end, self.y_step = -0.7, 0.3, 0.1
+        self.z_start, self.z_end, self.z_step = 0.72, 1.42, 0.1
+        
+        self.x = self.x_start - self.x_step  # Start before the first value
+        self.y = self.y_start
+        self.z = self.z_start
+        self.first_pass = True
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.x < self.x_end:
+            self.x += self.x_step
+        elif self.y < self.y_end:
+            self.y += self.y_step
+            self.x = self.x_start
+        elif self.z < self.z_end:
+            self.z += self.z_step
+            self.y = self.y_start
+            self.x = self.x_start
+        else:
+            if not self.first_pass:
+                raise StopIteration
+            self.first_pass = False
+
+        return round(self.x, 2), round(self.y, 2), round(self.z, 3)
+
 def target_experiment(index):
     calibration_matrix = [[0.45, -.05, 0.07],
                           [0.42, -0.0, 0.07],
